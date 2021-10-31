bab8e01a1a50cbf5474e0d9c7de74455a743adc4
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-10-30 22:02:04 -0300

    Apply repository pattern to avoid coupling

---
+++ PaymentContext/PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentContext.Domain.Repositories;
+
+public interface IStudentRepository
+{
+    bool DocumentExists(string document);
+    bool EmailExists(string email);
+    void CreateSubscription(Student student);
+}
